bf5a1253565cd1e544c23c44251c32e5669d84e6
Yurii Koltso <yurkoga25@gmail.com>
2019-03-27 20:54:36 +0200

    Portfolio v.0.3.8

---
+++ src/assets/js/chunk-2d0cc94e.c53e9dd3.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cc94e"],{"4f17":function(t,e,n){"use strict";n.r(e);var c=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("section",{staticClass:"section-title"},[n("h2",{staticClass:"section-title__text"},[t._v("Послуги")])])}],i=n("2877"),l={},a=Object(i["a"])(l,c,s,!1,null,null,null);e["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0cc94e.c53e9dd3.js.map
