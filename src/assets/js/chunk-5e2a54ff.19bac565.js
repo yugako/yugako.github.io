dc71ef15910ce4f9695feace45ca32398d222c29
Yurii Koltso <yurkoga25@gmail.com>
2019-03-29 17:02:23 +0200

    Testing prefetch

--- src/assets/js/chunk-5e2a54ff.19bac565.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-5e2a54ff"],{"08c3":function(n,t,c){},9703:function(n,t,c){"use strict";c.r(t);var e=function(){var n=this,t=n.$createElement,c=n._self._c||t;return c("h1",[n._v("Not found")])},u=[],f=(c("ff56"),c("2877")),o={},a=Object(f["a"])(o,e,u,!1,null,null,null);t["default"]=a.exports},ff56:function(n,t,c){"use strict";var e=c("08c3"),u=c.n(e);u.a}}]);
-//# sourceMappingURL=chunk-5e2a54ff.19bac565.js.map
