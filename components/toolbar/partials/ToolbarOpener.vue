113457cce3a06f5582174104623d7805a8ceee03
George <yurko25@outlook.com>
2019-11-03 15:06:34 +0200

    Fix menu burger

--- components/toolbar/partials/ToolbarOpener.vue
+++ components/toolbar/partials/ToolbarOpener.vue
@@ -1,7 +1,16 @@
 <template>
-    <div class="toolbar-opener" :class='{"menu-active": IsMenuOpened}' @click='menuOpen'>
-        <i v-if='!IsMenuOpened' class="fas fa-bars"></i>
-        <i v-else class="fas fa-times"></i>
+    <div class="toolbar-opener" :class='{"menu-active": IsMenuOpened}' >
+        <!-- <i v-if='!IsMenuOpened' class="fas fa-bars"></i>
+        <i v-else class="fas fa-times"></i> -->
+        <input type="checkbox" @click='menuOpen' checked='true' id="checkbox2" class="checkbox2 visuallyHidden">
+        <label for="checkbox2">
+            <div class="hamburger hamburger2">
+                <span class="bar bar1"></span>
+                <span class="bar bar2"></span>
+                <span class="bar bar3"></span>
+                <span class="bar bar4"></span>
+            </div>
+        </label>
     </div>
 </template>
 <script>
@@ -35,7 +44,7 @@
 	      	position: fixed;
 	      	background-color: rgba($dark, .4);
 	      	width: 40px;
-	      	height: 50px;
+	      	height: 40px;
 	      	top: 0;
 	      	left: 0px;
 	      	color: $white;
@@ -47,6 +56,68 @@
 	      	}
 	      	@include sm-size-max {
 	          	display: block;
+	      	}
+
+	      	/* GENERAL STYLES FOR HAMBURGER */
+	      	.visuallyHidden {
+	      	    position: absolute; 
+	      	    overflow: hidden; 
+	      	    clip: rect(0 0 0 0); 
+	      	    height: 1px; width: 1px; 
+	      	    margin: -1px; padding: 0; border: 0; 
+	      	}
+
+	      	.hamburger {
+	      	    margin: 0 auto;
+	      	    margin-top: 5px;
+	      	    width: 30px;
+	      	    height: 30px;
+	      	    position: relative;
+	      	    .bar {
+	      	    	padding: 0;
+	      	    	width: 30px;
+	      	    	height: 4px;
+	      	    	background-color: $accent;
+	      	    	display: block;
+	      	    	border-radius: 4px;
+	      	    	transition: all 0.4s ease-in-out;
+	      	    	position: absolute; 
+	      	    }
+		      	.bar1 {
+		      	    top: 0;
+		      	}
+
+		      	.bar2,
+		      	.bar3 {
+		      	    top: 13.5px;
+		      	}
+
+		      	.bar3 {
+		      	    right: 0;
+		      	}
+
+		      	.bar4 {
+		      	    bottom: 0;
+		      	}
+	      	}
+	      	/* HAMBURGER 2 */
+
+	      	.checkbox2:checked + label > .hamburger2 > .bar1{
+	      	    transform: translateX(40px);
+	      	    background-color: transparent;
+	      	}
+
+	      	.checkbox2:checked + label > .hamburger2 > .bar2{
+	      	    transform: rotate(45deg);
+	      	}
+
+	      	.checkbox2:checked + label > .hamburger2 > .bar3{
+	      	    transform: rotate(-45deg);
+	      	}
+
+	      	.checkbox2:checked + label > .hamburger2 > .bar4{
+	      	    transform: translateX(-40px);
+	      	    background-color: transparent;
 	      	}
 	  	}
 	}
